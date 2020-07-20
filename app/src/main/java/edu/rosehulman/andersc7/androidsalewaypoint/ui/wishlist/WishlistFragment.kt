69c42edfce14eda048dba16300f7b096bc96f8f9
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-20 00:49:43 -0700

    Refactored temporary Game list code

--- app/src/main/java/edu/rosehulman/andersc7/androidsalewaypoint/ui/wishlist/WishlistFragment.kt
+++ app/src/main/java/edu/rosehulman/andersc7/androidsalewaypoint/ui/wishlist/WishlistFragment.kt
@@ -18,21 +18,8 @@ class WishlistFragment : Fragment() {
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		this.root = inflater.inflate(R.layout.fragment_wishlist, container, false)
 
-		val games = arrayListOf(Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(),
-				Game(), Game())
+		val games: ArrayList<Game> = ArrayList()
+		for (i in 0 until 18) games.add(Game())
 		this.adapter = GameAdapter(this.root.context, games)
 		val recyclerView = this.root.findViewById<RecyclerView>(R.id.recycler_view)
 		recyclerView.layoutManager = GridLayoutManager(this.root.context, 2)
