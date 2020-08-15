3bcb1d489f325ab246a178cc68098151b3a0ff5c
Zehren <petersjl@rose-hulman>
2020-08-15 01:56:35 -0400

    Game tiles now update store availability in real-time

--- app/src/main/java/edu/rosehulman/andersc7/androidsalewaypoint/ui/game/GameViewHolder.kt
+++ app/src/main/java/edu/rosehulman/andersc7/androidsalewaypoint/ui/game/GameViewHolder.kt
@@ -1,20 +1,96 @@
 package edu.rosehulman.andersc7.androidsalewaypoint.ui.game
 
+import android.graphics.Color
+import android.provider.SyncStateContract
+import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.*
+import edu.rosehulman.andersc7.androidsalewaypoint.Constants
+import edu.rosehulman.andersc7.androidsalewaypoint.ui.listing.Listing
+import edu.rosehulman.andersc7.androidsalewaypoint.ui.listing.StoreType
+import kotlinx.android.synthetic.main.item_game.view.*
+import kotlinx.android.synthetic.main.nav_header_main.view.*
 
-class GameViewHolder : RecyclerView.ViewHolder {
-	var adapter: GameAdapter
-	var view: View
-
-	constructor(itemView: View, adapter: GameAdapter): super(itemView) {
-		this.adapter = adapter
-		this.view = itemView
-	}
+class GameViewHolder(itemView: View, var adapter: GameAdapter) : RecyclerView.ViewHolder(itemView) {
+	var view: View = itemView
+	var listingsListener: ListenerRegistration? = null
 
 	fun bind(game: Game) {
 		this.itemView.setOnClickListener {
 			this.adapter.selectGameAt(this.adapterPosition)
 		}
+		setIconsBlack()
+		Log.d(Constants.TAG, game.listings.toString())
+		listingsListener = FirebaseFirestore
+			.getInstance()
+			.collection("Games")
+			.document(game.id)
+			.collection("Listings").addSnapshotListener { listings: QuerySnapshot?, error: FirebaseFirestoreException? ->
+				if (error != null) Log.d(Constants.TAG, "Well somthing went wrong in the GameViewHolder")
+				else{
+					setIconsBlack()
+					for(l in listings!!){
+						val listing = l.toObject(Listing::class.java)
+						when(listing.store){
+							StoreType.STEAM -> {
+								DrawableCompat.setTint(
+									DrawableCompat.wrap(view.tile_steam.drawable),
+									Color.parseColor(if (listing.sale == 0f) "#EEEEEE" else "#00A273")
+								)
+							}
+							StoreType.PLAYSTATION -> {
+								DrawableCompat.setTint(
+									DrawableCompat.wrap(view.tile_playstation.drawable),
+									Color.parseColor(if (listing.sale == 0f) "#EEEEEE" else "#00A273")
+								)
+							}
+							StoreType.XBOX -> {
+								DrawableCompat.setTint(
+									DrawableCompat.wrap(view.tile_xbox.drawable),
+									Color.parseColor(if (listing.sale == 0f) "#EEEEEE" else "#00A273")
+								)
+							}
+							StoreType.NINTENDO -> {
+								DrawableCompat.setTint(
+									DrawableCompat.wrap(view.tile_nintendo.drawable),
+									Color.parseColor(if (listing.sale == 0f) "#EEEEEE" else "#00A273")
+								)
+							}
+							StoreType.ITCH -> {
+								DrawableCompat.setTint(
+									DrawableCompat.wrap(view.tile_itch.drawable),
+									Color.parseColor(if (listing.sale == 0f) "#EEEEEE" else "#00A273")
+								)
+							}
+						}
+					}
+				}
+			}
+	}
+
+	fun setIconsBlack(){
+		DrawableCompat.setTint(
+			DrawableCompat.wrap(view.tile_steam.drawable),
+			Color.parseColor("#000000")
+		)
+		DrawableCompat.setTint(
+			DrawableCompat.wrap(view.tile_playstation.drawable),
+			Color.parseColor("#000000")
+		)
+		DrawableCompat.setTint(
+			DrawableCompat.wrap(view.tile_xbox.drawable),
+			Color.parseColor("#000000")
+		)
+		DrawableCompat.setTint(
+			DrawableCompat.wrap(view.tile_nintendo.drawable),
+			Color.parseColor("#000000")
+		)
+		DrawableCompat.setTint(
+			DrawableCompat.wrap(view.tile_itch.drawable),
+			Color.parseColor("#000000")
+		)
 	}
 }
