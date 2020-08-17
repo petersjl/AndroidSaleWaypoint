81b1de21cd743de84f6242dac520e0833a7f34aa
Cooper Anderson <cooper@cooperstuff.ninja>
2020-08-16 23:11:30 -0700

    Added default image if image loading fails

--- app/src/main/java/edu/rosehulman/andersc7/androidsalewaypoint/ui/ImageTask.kt
+++ app/src/main/java/edu/rosehulman/andersc7/androidsalewaypoint/ui/ImageTask.kt
@@ -3,12 +3,21 @@ package edu.rosehulman.andersc7.androidsalewaypoint.ui
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.AsyncTask
+import edu.rosehulman.andersc7.androidsalewaypoint.Constants
+import java.lang.IllegalStateException
 import java.net.URL
 
 class ImageTask(private var imageConsumer: ImageConsumer) : AsyncTask<String, Void, Bitmap>() {
 	override fun doInBackground(vararg params: String?): Bitmap {
-		val inStream = URL(params[0]).openStream()
-		return BitmapFactory.decodeStream(inStream)
+		var inStream = URL(params[0]).openStream()
+		val bitmap: Bitmap
+		try {
+			bitmap = BitmapFactory.decodeStream(inStream)
+		} catch (e: IllegalStateException) {
+			inStream = URL(Constants.INVALID_IMAGE).openStream()
+			return BitmapFactory.decodeStream(inStream)
+		}
+		return bitmap
 	}
 
 	override fun onPostExecute(result: Bitmap?) {
